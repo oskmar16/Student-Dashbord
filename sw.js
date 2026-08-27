1c9b24775feb284847ec098f5d1efa8c15b92852
Oskar Mardal <oskar@mardal.net>
2026-08-27 13:25:48 +0200

    Show all seven days in week calendar

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="min-hverdag-v15";
+const CACHE_NAME="min-hverdag-v16";
 const APP_SHELL=["./","./index.html","./styles.css","./app.js","./cloud-sync.js","./pwa.js","./manifest.webmanifest?v=20260827-2","./icons/icon.svg?v=20260827-2","./icons/min-hverdag-192.png","./icons/min-hverdag-512.png"];
 self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
