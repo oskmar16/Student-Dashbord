a4b8d58f7a317f5f84bad471f7116b7b19204253
Oskar Mardal <oskar@mardal.net>
2026-08-27 11:49:31 +0200

    Refresh installed app icon assets

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME="min-hverdag-v8";
-const APP_SHELL=["./","./index.html","./styles.css","./app.js","./cloud-sync.js","./pwa.js","./manifest.webmanifest","./icons/icon.svg","./icons/icon-192.png","./icons/icon-512.png"];
+const CACHE_NAME="min-hverdag-v9";
+const APP_SHELL=["./","./index.html","./styles.css","./app.js","./cloud-sync.js","./pwa.js","./manifest.webmanifest?v=20260827-2","./icons/icon.svg?v=20260827-2","./icons/min-hverdag-192.png","./icons/min-hverdag-512.png"];
 self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",event=>{
